d88d20a26dcc307d1b564fde1a2979e9fc39c236
Yuki Yasoshima <yukiyasos@gmail.com>
2017-03-04 14:16:45 +0900

    add files

---
+++ x3f_mac_viewer/x3f_mac_viewer/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  x3f_mac_viewer
+//
+//  Created by Yuki Yasoshima on 2017/03/04.
+//  Copyright © 2017年 Yuki Yasoshima. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    var directoryUrl: URL?
+    var selectedFileUrl: URL?
+}
---
+++ x3f_mac_viewer/x3f_mac_viewer/RootViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RootViewController.swift
+//  x3f_mac_viewer
+//
+//  Created by Yuki Yasoshima on 2017/03/04.
+//  Copyright © 2017年 Yuki Yasoshima. All rights reserved.
+//
+
+import Cocoa
+
+class RootViewController : NSSplitViewController {
+    override func prepare(for segue: NSStoryboardSegue, sender: Any?) {
+        
+    }
+}
