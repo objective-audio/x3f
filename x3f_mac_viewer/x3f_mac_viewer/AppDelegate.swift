1f8bc6266af882340f9d0be6b27528c2b9464aa3
Yuki Yasoshima <yukiyasos@gmail.com>
2017-03-04 10:25:53 +0900

    create project

---
+++ x3f_mac_viewer/x3f_mac_viewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  x3f_mac_viewer
+//
+//  Created by Yuki Yasoshima on 2017/03/04.
+//  Copyright © 2017年 Yuki Yasoshima. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ x3f_mac_viewer/x3f_mac_viewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  x3f_mac_viewer
+//
+//  Created by Yuki Yasoshima on 2017/03/04.
+//  Copyright © 2017年 Yuki Yasoshima. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
